4c6fb564cfe431916e8ad901bb1c3f37a9901810
alfat2 <mnuralfat232.2@gmail.com>
2022-09-20 16:02:08 +0700

    pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> perkenalkan</h1>";
+        echo"Nama saya Muhammad Nur Alfat
+            Saya tinggal di daerah Cikampek
+            Olahraga yang saya sukai adalah
+            Bulu Tangkis";
+    }
+}
